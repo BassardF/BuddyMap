456ab3053ba1b497650e515f73bd9e8843c99cab
frank bassard <f.bassard@gmail.com>
2014-06-16 19:19:33 +0200

    Login moves.
    Event and move to open log in forms.

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -4,4 +4,14 @@ phonecatControllers.controller('LoginCtrl', function ($scope) {
   };
   $scope.showFacebookForm = false;
   $scope.showLoginForm = false;
+
+  $scope.loginClick = function(){
+    $scope.showLoginForm = true;
+    $scope.showFacebookForm = false;
+  }
+
+  $scope.facebookClick = function(){
+    $scope.showLoginForm = false;
+    $scope.showFacebookForm = true;
+  }
 });
