3bfc10072ad842cb9e9355851f19881bcd7c3ba9
frank bassard <f.bassard@gmail.com>
2014-06-19 20:46:46 +0200

    Login page V2.
    Visual improvement and added features

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -1,17 +1,92 @@
-phonecatControllers.controller('LoginCtrl', function ($scope) {
+/*
+* Controller of the login page
+*/
+phonecatControllers.controller('LoginCtrl', function ($scope, $http, $location, $rootScope, facebook) {
   $scope.labels = {
-    brand : "Team Up"
+    login : "Log in",
+    loginSub : "Already member ?",
+    loginMailLabel : "Email or Login",
+    loginPasswordLabel : "Password",
+    loginValidate : "Log in",
+    register : "Register",
+    registerSub : "Create your account",
+    facebook : "Facebook",
+    facebookSub  : "Register made easy",
+    facebookMail : "Email",
+    facebookPassword  : "Password",
+    facebookValidate : "Log in / Register"
   };
-  $scope.showFacebookForm = false;
-  $scope.showLoginForm = false;
 
-  $scope.loginClick = function(){
-    $scope.showLoginForm = true;
-    $scope.showFacebookForm = false;
+  /*
+  * Checking if the user is known in the localStorage
+  * If he is, upload his datas in the rootscope and redirect to the home
+  */
+  $scope.init = function(){
+    if (Modernizr.localstorage) {
+      var user = localStorage.getItem("user");
+      if(user){
+        $rootScope.user = user;
+        $location.path("/home");
+      }
+    }
   }
 
+  //Access to the basic login form
+  $scope.loginClick = function(){
+    $scope.showLoginForm = !$scope.showLoginForm;
+    $scope.showFacebookForm = false;
+    if($scope.showLoginForm){
+      $scope.loginBackground = "darkBackground";
+    } else{
+      $scope.loginBackground = "";
+    }
+    $scope.facebookBackground = "";
+  }
+
+  //Redirect to the register page
+  $scope.registerClick= function(){
+    $location.path("/register");
+  }
+
+  //Access to the facebook login/register form
   $scope.facebookClick = function(){
     $scope.showLoginForm = false;
-    $scope.showFacebookForm = true;
+    $scope.showFacebookForm = !$scope.showFacebookForm;
+    if($scope.showFacebookForm){
+      $scope.facebookBackground = "darkBackground";
+    } else{
+      $scope.facebookBackground = "";
+    }
+    $scope.loginBackground = "";
+  }
+
+  //Submit the basic login form
+  $scope.basicLogin = function(){
+    var config = {
+      method : "",
+      url : "",
+      params : {
+        login : scope.loginMail,
+        password : scope.loginPassword
+      }
+    };
+    $http(config).success(function(data) {
+      
+    });
+  }
+
+  //Submit the facebook form
+  $scope.facebookLogin = function(){
+    var config = {
+      method : "",
+      url : "",
+      params : {
+        login :scope.facebookMail,
+        password : scope.facebookPassword
+      }
+    };
+    $http(config).success(function(data) {
+      
+    });
   }
 });
