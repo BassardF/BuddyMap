3c16bdc986366381263adefcc11f255b191b66b9
VierenV <valentin.vieren@gmail.com>
2014-07-17 11:37:22 +0200

    Creation service event

---
+++ www/js/services/event.js
@@ -0,0 +1,30 @@
+/*
+* Event service
+*/
+phonecatControllers.service('event', ['$http', 'crypto', function($http, crypto) {
+	
+	this.baseUrl = 'http://ec2-54-191-70-54.us-west-2.compute.amazonaws.com/buddyMap';
+	
+	this.retrieveEvents = function(mail, token){
+		var clientTimestamp = Date.now();
+		var authorizationHeader = {
+			"clientTimestamp" : clientTimestamp,
+			"mail" : mail, 
+			"hashSignature" : crypto.hashSHA256(mail+clientTimestamp+crypto.getHashAlgorythmUsed(), token),
+			"hashAlgorythm" : "SHA256"	
+		}
+		var config = {
+		    	method : "GET",
+		    	url : this.baseUrl + "/events?mail=" + mail,
+		    	headers : {
+		    		"Authorization" : authorizationHeader;
+		    	}
+		    };
+		 $http(config).success(function() {
+			 alert("OK");
+		 }).
+		 error(function(data, status, headers, config) {
+			 alert("KO "+status);
+		 });
+	}
+});
